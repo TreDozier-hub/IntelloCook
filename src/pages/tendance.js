57507b1032effede4b74770bf7ec86be3ae388b7
TreDozier-hub <arnaud.dozier@gmail.com>
2024-11-24 22:34:21 +0100

    Mise à jour code header

---
+++ src/pages/tendance.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Header from "./components/Header.js";
+import "./components/Accueil.css";
+
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+        <>
+          <Header />
+        </>
+      </header>
+
+      <main>
+
+        <div class="container">
+
+          
+
+          <section class="section-2">
+            
+          </section>
+
+        </div>
+
+        
+
+      </main>
+
+      <footer>
+
+      </footer>
+
+    </div>
+
+
+
+
+  );
+}
+
+export default App;
