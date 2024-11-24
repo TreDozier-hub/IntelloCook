64a1ee49f55dce0900879313a6fd40e886461e62
TreDozier-hub <arnaud.dozier@gmail.com>
2024-11-24 17:51:03 +0100

    Update - mise à jour de l'accueil et de heacer par rapport à la charte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header.js";
+import Image_1 from "./assets/machine-01.png";
+import Image_2 from "./assets/logo.png";
+import "./components/Accueil.css";
+
 
 function App() {
   return (
+
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>
+          <Header />
+        </>
       </header>
+
+      <main>
+
+        <div class="container">
+
+          <section class="section-1">
+
+            <div class="imageGauche">
+              <img src={Image_1} alt="Robot Cuisine" class="robot-image" />
+            </div>
+
+            <div class="logoAccueil">
+              <img src={Image_2} alt="Logo IntelliCook" class="logo2" />
+              <h1 class="sousTitre">Des recettes faciles <br /> pour cuisiner Simplement</h1>
+              <hr className="separator" />
+              <p class="description">
+                Notre plateforme vous permettra d’avoir un choix illimité de recettes en fonction des ingrédients de votre choix.
+              </p>
+
+              {/* Pas encore de lien derriere */}
+              <button class="buttonRecette">Commencer</button>
+              <br />
+              <br />
+            </div>
+          </section>
+
+          <section class="section-2">
+            {/* <Recettes /> */}
+          </section>
+
+        </div>
+
+        {/* <Card /> */}
+
+      </main>
+
+      <footer>
+
+      </footer>
+
     </div>
+
+
+
+
   );
 }
 
