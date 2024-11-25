0b3936e32061dd035680f78629ae7be5c743de47
TreDozier-hub <arnaud.dozier@gmail.com>
2024-11-26 00:09:01 +0100

    Update - Corrections Bugs

--- src/components/Recettes.js
+++ src/components/Recettes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import RecetteSimple from "./RecetteSimple";
+// import RecetteSimple from "./RecetteSimple";
 import data from "../recettesList.json";
 import "./RecetteSimple.css";
 
